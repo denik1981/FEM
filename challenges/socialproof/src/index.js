ea9899879f679fd9c65c2761146da83ac7142781
denik1981 <demian.m1981@gmail.com>
2021-06-17 22:26:36 -0300

    First finished version

--- challenges/socialproof/src/index.js
+++ challenges/socialproof/src/index.js
@@ -1,1 +1,2 @@
-import './styles.css';
+import './tailwind.css'
+import './styles.css'
--- challenges/socialproof/tailwind.config.js
+++ challenges/socialproof/tailwind.config.js
@@ -11,26 +11,10 @@ module.exports = {
       fontFamily: {
         primary: ['Spartan', ...defaultTheme.fontFamily.sans]
       }
-      // gridTemplateColumns: {
-      // desktop: '60% 40%',
-      // },
-
-      // fontSize: {
-      //   fluidh: 'clamp(1.2rem, 5vw, 2rem)',
-      // },
       // backgroundImage: () => ({
       // myHeroDesktop: "url('~assets/bg-desktop.svg')",
       // myHeroMobile: "url('~assets/bg-mobile.svg')",
       // }),
-      // maxWidth: {
-      // '20ch': '20ch',
-      // },
-      // boxShadow: {
-      // btn: '0 7px 5px -2px rgba(0,0,0,0.3)',
-      // },
-      // outline: {
-      // magenta: ['1px dashed #22183A', '6px'],
-      // },
     }
   },
   variants: {
