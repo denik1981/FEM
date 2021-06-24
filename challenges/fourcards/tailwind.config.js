e5e497028ff9d2a9406edcdb3d2e2a74d42f2851
denik1981 <demian.m1981@gmail.com>
2021-06-24 03:36:07 -0300

    Tailwind + BEM. First Draft.

--- challenges/fourcards/tailwind.config.js
+++ challenges/fourcards/tailwind.config.js
@@ -3,14 +3,27 @@ module.exports = {
   purge: ['./index.ejs'],
   theme: {
     extend: {
-      // colors: {
-      //   primary: { DEFAULT: 'var(--cl-primary)', light: 'var(--cl-primary-light)' },
-      //   secondary: { DEFAULT: 'var(--cl-secondary)' },
-      //   neutral: { DEFAULT: 'var(--cl-neutral)', light: 'var(--cl-neutral-light)' }
-      // },
+      colors: {
+        primary: {
+          red: 'var(--cl-red)',
+          cyan: 'var(--cl-cyan)',
+          orange: 'var(--cl-orange)',
+          blue: 'var(--cl-blue)'
+        },
+        neutral: {
+          300: 'var(--cl-neutral-300)',
+          600: 'var(--cl-neutral-600)'
+        }
+      },
       fontFamily: {
         primary: ['var(--ff-primary)', ...defaultTheme.fontFamily.sans]
-      }
+      },
+      backgroundImage: theme => ({
+        supervisor: 'var(--bg-icon-supervisor)',
+        teambuilder: 'var(--bg-icon-teambuilder)',
+        karma: 'var(--bg-icon-karma)',
+        calculator: 'var(--bg-icon-calculator)'
+      })
     }
   }
 }
