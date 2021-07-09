4c0001b041f196a9e900fd67c6dbe8cdcfe3a544
denik1981 <demian.m1981@gmail.com>
2021-07-09 13:41:08 -0300

    build: Add all the necessary configuration files.

---
+++ challenges/flyodarklandingpage/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const isProduction = process.env.NODE_ENV === 'production'
+const stylesHandler = isProduction ? MiniCssExtractPlugin.loader : 'style-loader'
+const cssLoader = { loader: 'css-loader', options: { sourceMap: true } }
+const publicPath = isProduction && !(process.env.WEBPACK_DEV_SERVER) ? path.sep + path.basename(__dirname) + path.sep : ''
+
+const config = {
+  mode: isProduction ? 'production' : 'development',
+  stats: 'detailed',
+  devtool: 'source-map',
+  devServer: { host: 'localhost' },
+  optimization: { runtimeChunk: 'single' },
+
+  entry: { main: path.resolve(__dirname, 'src/index.js') },
+  output: {
+    clean: isProduction && !(process.env.WEBPACK_DEV_SERVER),
+    path: isProduction ? path.resolve('..', '..', 'public', path.basename(__dirname)) : path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'assets/[hash][ext]',
+    publicPath: publicPath
+  },
+
+  resolve: { alias: { assets: path.resolve(__dirname, 'assets') } },
+
+  module: {
+    rules: [
+      { test: /\.css$/i, use: [stylesHandler, cssLoader, 'postcss-loader'] },
+      { test: /\.(|svg|png|jpg|gif)$/i, type: 'asset/resource' }
+    ]
+  },
+  plugins: []
+}
+
+module.exports = (env) => {
+  config.plugins.push(new HtmlWebpackPlugin({ minify: false, favicon: 'assets/misc/favicon.png', template: 'index.html' }))
+  if (isProduction) config.plugins.push(new MiniCssExtractPlugin({ filename: '[contenthash].css' }))
+  return config
+}
