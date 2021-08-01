6fce292b55753dbf928b332f66f28f0dd6271804
denik1981 <demian.m1981@gmail.com>
2021-08-01 18:55:32 -0300

    feat: Add a little animation at window.onload time

--- challenges/testimonialsgridsection/src/index.js
+++ challenges/testimonialsgridsection/src/index.js
@@ -1,2 +1,15 @@
 import './css/_vendors.css'
 import './css/main.css'
+
+// This prevents the animation from running again when the user resizes
+// Or when the user switch from portrait to landscape
+// It also disables the animation on portrait devices of less that 1280px width
+
+function removeAnimation () {
+  const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+  const testimonials = [...document.getElementsByClassName('testimonial')]
+  if (vw <= 1280) { testimonials.forEach(testimonial => testimonial.classList.remove('animate')) }
+  testimonials.forEach(v => v.addEventListener('animationend', e => e.target.classList.remove('animate')))
+}
+
+window.onload = removeAnimation
