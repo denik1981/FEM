efdf038abcee4e98e07257b742adeae717c87115
denik1981 <demian.m1981@gmail.com>
2021-08-04 13:55:06 -0300

    feat: Remove Browser Defaults Events for the Details/Summary tags

--- challenges/faqaccordioncard/src/index.js
+++ challenges/faqaccordioncard/src/index.js
@@ -1,3 +1,21 @@
-// document.querySelector('.a1').scrollHeight
 import './css/_vendors.css'
 import './css/main.css'
+
+const settings = {
+  fallback: 'fallbackNoJS'
+}
+
+function init () {
+  const { fallback } = settings
+  document.querySelector(`[${fallback}]`).removeAttribute(fallback)
+  preventDefaults('summary', ['onclick'])
+}
+
+function preventDefaults (selector, events) {
+  const elements = document.querySelectorAll(selector)
+  elements.forEach(element => {
+    [...events].forEach(event => element.addEventListener('click', (e) => e.preventDefault()))
+  })
+}
+
+window.onload = init
