7177258e0de076876ad2c25a9319d0557a585353
denik1981 <demian.m1981@gmail.com>
2021-08-09 16:43:50 -0300

    build: Remove not needed bundler analyser from testimonialgridsection.

--- challenges/testimonialsgridsection/webpack.config.js
+++ challenges/testimonialsgridsection/webpack.config.js
@@ -1,50 +1,49 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
-const isMonoBuild = process.env.MONO_BUILD ? process.env.MONO_BUILD === 'mono' : false
-const isProduction = process.env.NODE_ENV === 'production'
-const stylesHandler = isProduction ? MiniCssExtractPlugin.loader : 'style-loader'
-const publicPath = isProduction && !(process.env.WEBPACK_DEV_SERVER) ? path.sep + path.basename(__dirname) + path.sep : ''
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const isMonoBuild = process.env.MONO_BUILD ? process.env.MONO_BUILD === "mono" : false;
+const isProduction = process.env.NODE_ENV === "production";
+const stylesHandler = isProduction ? MiniCssExtractPlugin.loader : "style-loader";
+const publicPath =
+  isProduction && !process.env.WEBPACK_DEV_SERVER ? path.sep + path.basename(__dirname) + path.sep : "";
 
 const config = {
-  mode: isProduction ? 'production' : 'development',
-  stats: isMonoBuild ? 'summary' : 'detailed',
-  devtool: 'source-map',
-  devServer: { host: 'localhost' },
+  mode: isProduction ? "production" : "development",
+  stats: isMonoBuild ? "summary" : "detailed",
+  devtool: "source-map",
+  devServer: { host: "localhost" },
   optimization: {
-    runtimeChunk: 'single',
+    runtimeChunk: "single",
     minimize: isProduction,
-    minimizer: [
-      '...',
-      new CssMinimizerPlugin()
-    ]
+    minimizer: ["...", new CssMinimizerPlugin()],
   },
 
   entry: {
-    main: path.resolve(__dirname, 'src/index.js')
+    main: path.resolve(__dirname, "src/index.js"),
   },
   output: {
-    clean: isProduction && !(process.env.WEBPACK_DEV_SERVER),
-    path: isProduction ? path.resolve('..', '..', 'public', path.basename(__dirname)) : path.resolve(__dirname, 'dist'),
-    assetModuleFilename: 'assets/[hash][ext]',
-    publicPath: publicPath
+    clean: isProduction && !process.env.WEBPACK_DEV_SERVER,
+    path: isProduction ? path.resolve("..", "..", "public", path.basename(__dirname)) : path.resolve(__dirname, "dist"),
+    assetModuleFilename: "assets/[hash][ext]",
+    publicPath: publicPath,
   },
 
-  resolve: { alias: { assets: path.resolve(__dirname, 'assets') } },
+  resolve: { alias: { assets: path.resolve(__dirname, "assets") } },
 
   module: {
     rules: [
-      { test: /\.css$/i, use: [stylesHandler, 'css-loader', 'postcss-loader'] },
-      { test: /\.(|svg|png|jpg|gif)$/i, type: 'asset/resource' }
-    ]
+      { test: /\.css$/i, use: [stylesHandler, "css-loader", "postcss-loader"] },
+      { test: /\.(|svg|png|jpg|gif)$/i, type: "asset/resource" },
+    ],
   },
-  plugins: [new BundleAnalyzerPlugin()]
-}
+  plugins: [],
+};
 
 module.exports = (env) => {
-  config.plugins.push(new HtmlWebpackPlugin({ minify: false, favicon: 'assets/misc/favicon.png', template: 'index.html' }))
-  if (isProduction) config.plugins.push(new MiniCssExtractPlugin({ filename: '[contenthash].css' }))
-  return config
-}
+  config.plugins.push(
+    new HtmlWebpackPlugin({ minify: false, favicon: "assets/misc/favicon.png", template: "index.html" })
+  );
+  if (isProduction) config.plugins.push(new MiniCssExtractPlugin({ filename: "[contenthash].css" }));
+  return config;
+};
