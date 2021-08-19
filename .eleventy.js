ce69656a12f8c974180dc10afd243855390891ba
denik1981 <demian.m1981@gmail.com>
2021-08-19 19:03:50 -0300

    build: Add 11ty with basic configuration

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const path = require("path");
+const copyDirs = ["images", "assets"];
+const config = {
+  dir: { input: "demo", output: path.join("public", "demo") },
+  templateFormats: ["html", "liquid", "njk"],
+};
+
+module.exports = function (eleventyConfig) {
+  fs.rmSync(config.dir.output, { recursive: true, force: true });
+  copyDirs.forEach((dir) =>
+    eleventyConfig.addPassthroughCopy(path.join(config.dir.input, dir))
+  );
+  return config;
+};
